d90ee608bfd6e8a1d401e5eaa1a63f9a63e3be80
Raudel Flores <rawdell@outlook.com>
2020-01-21 22:16:50 -0600

    finished creating the darkmode function that toggles dark mode on the page

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('true');
 
+    useEffect(() => {
+        if (someValue === true){
+            return document.body.classList.add('dark-mode'); 
+        }
+       return document.body.classList.remove('dark-mode')
+    }, [someValue]);
+
+    return [someValue, setSomeValue];
 }
